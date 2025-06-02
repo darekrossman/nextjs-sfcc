2001cba075c94e34f0d809f367e99fbc777e2c0f
Darek Rossman <darekrossman@me.com>
2025-06-01 22:58:51 -0400

    sync up

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,15 +1,15 @@
 'use client'
 
-// import { assist } from '@sanity/assist'
+import { assist } from '@sanity/assist'
 import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
-// import { presentationTool } from 'sanity/presentation'
+import { presentationTool } from 'sanity/presentation'
 import { internationalizedArray } from 'sanity-plugin-internationalized-array'
 import { apiVersion, dataset, projectId } from './sanity/env'
 import { schema } from './sanity/schemaTypes'
 import { structure } from './sanity/structure.config'
-// import { resolve } from './sanity/presentation/resolve'
+import { resolve } from './sanity/presentation/resolve'
 
 export default defineConfig({
   basePath: '/studio',
@@ -17,28 +17,28 @@ export default defineConfig({
   dataset,
   schema,
   plugins: [
-    // assist({
-    //   translate: {
-    //     field: {
-    //       languages: (client) =>
-    //         client.fetch(`*[_type == "locale"]{"id":tag, "title":name}`),
-    //       documentTypes: ['siteSettings', 'page', 'category'],
-    //     },
-    //   },
-    // }),
+    assist({
+      translate: {
+        field: {
+          languages: (client) =>
+            client.fetch(`*[_type == "locale"]{"id":tag, "title":name}`),
+          documentTypes: ['siteSettings', 'page', 'category'],
+        },
+      },
+    }),
     structureTool({ structure }),
     visionTool({ defaultApiVersion: apiVersion }),
     internationalizedArray({
       languages: (client) => client.fetch(`*[_type == "locale"]{"id":tag, "title":name}`),
       fieldTypes: ['string'],
     }),
-    // presentationTool({
-    //   resolve,
-    //   previewUrl: {
-    //     previewMode: {
-    //       enable: '/api/draft-mode/enable',
-    //     },
-    //   },
-    // }),
+    presentationTool({
+      resolve,
+      previewUrl: {
+        previewMode: {
+          enable: '/api/draft-mode/enable',
+        },
+      },
+    }),
   ],
 })
