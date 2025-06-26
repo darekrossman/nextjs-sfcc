bf18799c2e9fe9eb522079c6885edf7396b15b3b
Darek Rossman <darekrossman@me.com>
2025-06-26 11:08:03 -0400

    og

--- app/[locale]/(main)/product/[productId]/opengraph-image.tsx
+++ app/[locale]/(main)/product/[productId]/opengraph-image.tsx
@@ -1,5 +1,34 @@
-import OpengraphImage from 'components/opengraph-image'
+import { ImageResponse } from 'next/og'
+import LogoIcon from '@/components/icons/logo-animated'
+import { notFound } from 'next/navigation'
+import { getProduct } from '@/lib/sfcc'
+import { getProductImagesForColor } from '@/lib/sfcc/product-helpers'
 
-export default async function Image() {
-  return await OpengraphImage()
+export type Props = {
+  params: Promise<{ productId: string; locale: 'en' | 'fr' }>
+}
+
+export default async function Image(props: Props): Promise<ImageResponse> {
+  const params = await props.params
+  const product = await getProduct({ id: params.productId, locale: params.locale })
+
+  if (!product) return notFound()
+
+  const image = getProductImagesForColor(product.imageGroups)?.[0]
+
+  return new ImageResponse(
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      {image && <img src={image.link} alt={image.alt || ''} height="100" />}
+    </div>,
+    {
+      width: 1200,
+      height: 630,
+    },
+  )
 }
